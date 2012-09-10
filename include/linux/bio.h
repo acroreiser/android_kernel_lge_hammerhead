56a6950ca86acf2e9a8c754b82ebcdfc116f5409
Kent Overstreet <koverstreet@google.com>
2012-09-10 14:41:12 -0700

    BACKPORT: block: Add submit_bio_wait(), remove from md
    Random cleanup - this code was duplicated and it's not really specific
    to md.
    
    Also added the ability to return the actual error code.
    
    Signed-off-by: Kent Overstreet <koverstreet@google.com>
    CC: Jens Axboe <axboe@kernel.dk>
    CC: NeilBrown <neilb@suse.de>
    Acked-by: Tejun Heo <tj@kernel.org>

--- include/linux/bio.h
+++ include/linux/bio.h
@@ -221,6 +221,7 @@ extern void bio_free(struct bio *, struct bio_set *);
 extern void bio_endio(struct bio *, int);
 struct request_queue;
 extern int bio_phys_segments(struct request_queue *, struct bio *);
+extern int submit_bio_wait(int rw, struct bio *bio);
 
 extern void __bio_clone(struct bio *, struct bio *);
 extern struct bio *bio_clone(struct bio *, gfp_t);
