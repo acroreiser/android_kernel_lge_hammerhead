2aa85b35de6076baf596e94df4cc625bf9032a6b
Eric Paris <eparis@redhat.com>
2013-12-02 11:24:19 +0000

    security: shmem: implement kernel private shmem inodes
    We have a problem where the big_key key storage implementation uses a
    shmem backed inode to hold the key contents.  Because of this detail of
    implementation LSM checks are being done between processes trying to
    read the keys and the tmpfs backed inode.  The LSM checks are already
    being handled on the key interface level and should not be enforced at
    the inode level (since the inode is an implementation detail, not a
    part of the security model)
    
    This patch implements a new function shmem_kernel_file_setup() which
    returns the equivalent to shmem_file_setup() only the underlying inode
    has S_PRIVATE set.  This means that all LSM checks for the inode in
    question are skipped.  It should only be used for kernel internal
    operations where the inode is not exposed to userspace without proper
    LSM checking.  It is possible that some other users of
    shmem_file_setup() should use the new interface, but this has not been
    explored.
    
    Reproducing this bug is a little bit difficult.  The steps I used on
    Fedora are:
    
     (1) Turn off selinux enforcing:
    
    	setenforce 0
    
     (2) Create a huge key
    
    	k=`dd if=/dev/zero bs=8192 count=1 | keyctl padd big_key test-key @s`
    
     (3) Access the key in another context:
    
    	runcon system_u:system_r:httpd_t:s0-s0:c0.c1023 keyctl print $k >/dev/null
    
     (4) Examine the audit logs:
    
    	ausearch -m AVC -i --subject httpd_t | audit2allow
    
    If the last command's output includes a line that looks like:
    
    	allow httpd_t user_tmpfs_t:file { open read };
    
    There was an inode check between httpd and the tmpfs filesystem.  With
    this patch no such denial will be seen.  (NOTE! you should clear your
    audit log if you have tested for this previously)
    
    (Please return you box to enforcing)
    
    Change-Id: I4ff037291f99acd965ac759c2147b52e75078ceb
    Signed-off-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Hugh Dickins <hughd@google.com>
    cc: linux-mm@kvack.org

--- mm/shmem.c
+++ mm/shmem.c
@@ -3167,13 +3167,8 @@ int vmtruncate_range(struct inode *inode, loff_t lstart, loff_t lend)
 
 /* common code */
 
-/**
- * shmem_file_setup - get an unlinked file living in tmpfs
- * @name: name for dentry (to be seen in /proc/<pid>/maps
- * @size: size to be set for the file
- * @flags: VM_NORESERVE suppresses pre-accounting of the entire object size
- */
-struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags)
+static struct file *__shmem_file_setup(const char *name, loff_t size,
+				       unsigned long flags, unsigned int i_flags)
 {
 	int error;
 	struct file *file;
@@ -3206,6 +3201,7 @@ struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags
 	if (!inode)
 		goto put_dentry;
 
+	inode->i_flags |= i_flags;
 	d_instantiate(path.dentry, inode);
 	inode->i_size = size;
 	clear_nlink(inode);	/* It is unlinked */
@@ -3228,6 +3224,32 @@ put_dentry:
 put_memory:
 	shmem_unacct_size(flags, size);
 	return ERR_PTR(error);
+}
+
+/**
+ * shmem_kernel_file_setup - get an unlinked file living in tmpfs which must be
+ * 	kernel internal.  There will be NO LSM permission checks against the
+ * 	underlying inode.  So users of this interface must do LSM checks at a
+ * 	higher layer.  The one user is the big_key implementation.  LSM checks
+ * 	are provided at the key level rather than the inode level.
+ * @name: name for dentry (to be seen in /proc/<pid>/maps
+ * @size: size to be set for the file
+ * @flags: VM_NORESERVE suppresses pre-accounting of the entire object size
+ */
+struct file *shmem_kernel_file_setup(const char *name, loff_t size, unsigned long flags)
+{
+	return __shmem_file_setup(name, size, flags, S_PRIVATE);
+}
+
+/**
+ * shmem_file_setup - get an unlinked file living in tmpfs
+ * @name: name for dentry (to be seen in /proc/<pid>/maps
+ * @size: size to be set for the file
+ * @flags: VM_NORESERVE suppresses pre-accounting of the entire object size
+ */
+struct file *shmem_file_setup(const char *name, loff_t size, unsigned long flags)
+{
+	return __shmem_file_setup(name, size, flags, 0);
 }
 EXPORT_SYMBOL_GPL(shmem_file_setup);
 
