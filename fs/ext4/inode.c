6974b404d60bba7a4126e65f8eec51b838965507
Michael Halcrow <mhalcrow@google.com>
2017-02-13 09:22:36 -0800

    ANDROID: ext4 crypto: Disables zeroing on truncation when there's no key
    When performing orphan cleanup on mount, ext4 may truncate pages.
    Truncation as currently implemented may require the encryption key for
    partial zeroing, and the key isn't necessarily available on mount.
    Since the userspace tools don't perform the partial zeroing operation
    anyway, let's just skip doing that in the kernel.
    
    This patch fixes a BUG_ON() oops.
    
    Bug: 35209576
    Change-Id: I2527a3f8d2c57d2de5df03fda69ee397f76095d7
    Signed-off-by: Michael Halcrow <mhalcrow@google.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3337,6 +3337,11 @@ static int ext4_block_zero_page_range(handle_t *handle,
 	struct page *page;
 	int err = 0;
 
+	/* If we are processing an encrypted inode during orphan list
+	 * handling */
+	if (ext4_encrypted_inode(inode) && !ext4_has_encryption_key(inode))
+		return 0;
+
 	page = find_or_create_page(mapping, from >> PAGE_CACHE_SHIFT,
 				   mapping_gfp_mask(mapping) & ~__GFP_FS);
 	if (!page)
