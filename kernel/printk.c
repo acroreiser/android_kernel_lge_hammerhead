f57c8b63082f08368177f2cbe2fdfe6d6e7f29aa
Laura Abbott <lauraa@codeaurora.org>
2026-07-25 22:26:28 +0300

    printk: Remove RTB logging
    RTB printk logging was useful on older targets but it is significantly
    less so these days. The current setup is also incompatible with other
    architectures. Remove the implementation in printk.c. If the feature
    is desired later, it can be reimpmented in a more generic way
    
    Change-Id: Ia6e4aeced075048fb7d88747cdae4e6768edd022
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>

--- kernel/printk.c
+++ kernel/printk.c
@@ -44,10 +44,6 @@
 
 #include <asm/uaccess.h>
 
-#ifdef CONFIG_MSM_RTB
-#include <mach/msm_rtb.h>
-#endif
-
 #define CREATE_TRACE_POINTS
 #include <trace/events/printk.h>
 
@@ -799,11 +795,6 @@ asmlinkage int printk(const char *fmt, ...)
 {
 	va_list args;
 	int r;
-#ifdef CONFIG_MSM_RTB
-	void *caller = __builtin_return_address(0);
-
-	uncached_logk_pc(LOGK_LOGBUF, caller, (void *)log_end);
-#endif
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
